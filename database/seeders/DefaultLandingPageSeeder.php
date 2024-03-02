d255a8fb49ba2efd14d04233df73770b0df0313e
Ugik DEV <ugik.dev@gmail.com>
2024-03-03 04:47:26 +0700

    hero bg default setup

--- database/seeders/DefaultLandingPageSeeder.php
+++ database/seeders/DefaultLandingPageSeeder.php
@@ -18,7 +18,11 @@ class DefaultLandingPageSeeder extends Seeder
     public function run(): void
     {
 
-        Hero::updateOrCreate(['text_1' => 'Selamat Datang di', 'text_2' => 'Mall Pelayanan<br>Publik', 'button' => 'N']);
+        Hero::create(['text_1' => 'Selamat Datang di', 'text_2' => 'Mall Pelayanan<br>Publik', 'button' => 'N']);
+        Hero::create(['text_1' => 'Selamat Datang di', 'text_2' => 'Mall Pelayanan<br>Publik', 'button' => 'N']);
+        Hero::create(['text_1' => 'Selamat Datang di', 'text_2' => 'Mall Pelayanan<br>Publik', 'button' => 'N']);
+        Hero::create(['text_1' => 'Selamat Datang di', 'text_2' => 'Mall Pelayanan<br>Publik', 'button' => 'N']);
+
         HeroIcon::updateOrCreate(['number' => '1', 'text' => 'Pemerintahan', 'icon' => 'flaticon-parthenon', 'link' => 'https://www.bangka.go.id/', 'button' => 'Y', 'button_type' => 'link']);
         HeroIcon::updateOrCreate(['number' => '2', 'text' => 'Bisnis & Industri', 'icon' => 'fa-solid fa-industry', 'link' => 'https://www.bangka.go.id/', 'button' => 'N', 'button_type' => 'link']);
         HeroIcon::updateOrCreate(['number' => '3', 'text' => 'Sosial dan Pemberdayaan', 'icon' => 'flaticon-parthenon', 'link' => 'https://www.bangka.go.id/', 'button' => 'N', 'button_type' => 'link']);
--- resources/views/landingPage/hero.blade.php
+++ resources/views/landingPage/hero.blade.php
@@ -3,7 +3,7 @@
         @foreach ($heroes as $hero)
             <div class="item">
                 <div class="item-slider-bg"
-                    style="background-image: url({{ $hero->image ? url('/storage/upload/hero/' . $hero->image) : url('assets/image/bg/1.jpg') }});background-repeat:no-repeat !important;
+                    style="background-image: url({{ $hero->image ? url('/storage/upload/hero/' . $hero->image) : url('assets/background/bg-' . str_pad(rand(1, 23), 2, '0', STR_PAD_LEFT) . '.jpg') }});background-repeat:no-repeat !important;
                     background-position: center center !important;">
                 </div>
                 <div class="container">
