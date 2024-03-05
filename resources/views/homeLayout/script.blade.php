34dd1bd056e23847997c9528ffcfd08bc32cfd64
Ugik DEV <ugik.dev@gmail.com>
2024-03-05 09:12:51 +0700

    preload

--- resources/views/homeLayout/script.blade.php
+++ resources/views/homeLayout/script.blade.php
@@ -6,7 +6,7 @@
 <script src="{{ asset('assets/vendor/counter-up/jquery.counterup.min.js') }}"></script>
 <script src="{{ asset('assets/vendor/youtube-popup/youtube-popup.jquery.js') }}"></script>
 <script src="{{ asset('assets/vendor/jquery-validation/jquery.validate.min.js') }}"></script>
-<script src="{{ asset('assets/js/theme.jsss') }}"></script>
+<script src="{{ asset('assets/js/theme.js') }}"></script>
 <script src="https://use.fontawesome.com/4f3417ca28.js"></script>
 
 {{-- <script src="{{ asset('assets/js/mega-menu.js') }}"></script> --}}
