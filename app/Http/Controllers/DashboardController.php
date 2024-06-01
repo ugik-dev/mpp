cc895ab739b59bca759ac746096355f9b9c0b455
Ugik DEV <ugik.dev@gmail.com>
2024-06-01 20:24:02 +0700

    inisialisasi avg dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -12,7 +12,10 @@ class DashboardController extends Controller
     public function index()
     {
         $skm = ['responden' => Survey::count(), 'total' => Survey::sum('respon')];
-        $skm['avg'] = $skm['total'] / $skm['responden'];
+        if (empty($skm['total'] || empty($skm['responden']))) {
+            $skm['avg'] = 0;
+        } else
+            $skm['avg'] = $skm['total']  / $skm['responden'];
         return view('panel/dashboard', compact('skm'));
     }
 }
